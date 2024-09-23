d668746c3e6c81f0ea0f658b3f513f24bc7243f9
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2024-09-23 21:56:56 +0530

    Selenium Season-1 notes and programs till Sep 23rd

---
+++ src/sep23rd/ChromeLaunch1.java
@@ -0,0 +1,14 @@
+package sep23rd;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeLaunch1 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\FLM15thJuly\\jars\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		new ChromeDriver();
+
+	}
+
+}
